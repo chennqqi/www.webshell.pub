cf7cd14d490a3d671562bc558be8096deabf5215
CHENQ <chennqqi@qq.com>
2016-10-21 15:37:23 +0800

    fix cnzz bugs

--- js/cnzz.js
+++ js/cnzz.js
@@ -1,2 +1,4 @@
 var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cspan  src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1260319782' type='text/javascript'%3E%3C/script%3E"));
+document.write(unescape("%3Cscript src='" + 
+			cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1260319782' 
+			type='text/javascript'%3E%3C/script%3E"));
