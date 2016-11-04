1fe689520fcbec9aa823a033280c3373588a4057
CHENQ <chennqqi@qq.com>
2016-11-04 11:19:52 +0800

    统计代码换https

--- js/cnzz.js
+++ js/cnzz.js
@@ -1,4 +1,3 @@
-var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cspan style='display:none;' id='cnzz_stat_icon_1260319782'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1260319782' type='text/javascript'%3E%3C/script%3E"));
+document.write(unescape("%3Cspan style='display:none;' id='cnzz_stat_icon_1260319782'%3E%3C/span%3E%3Cscript src='" + "https://s95.cnzz.com/z_stat.php%3Fid%3D1260319782' type='text/javascript'%3E%3C/script%3E"));
 document.getElementById("cnzz_stat_icon_1260319782").style.display = "none";
 
